a87f7282604577480cbb8f9c863415e8b66a04ff
RizWaaN3024 <rizwaanans786@gmail.com>
2025-07-06 16:42:59 +0530

    Added messages types

--- src/UserManager.ts
+++ src/UserManager.ts
@@ -10,16 +10,27 @@ interface Room {
 }
 
 export class UserManager {
-    private users: Map<string, Room>;
+    private rooms: Map<string, Room>;
     constructor() {
-        this.users = new Map<string, Room>();
+        this.rooms = new Map<string, Room>();
     }
 
     addUser(name: string, userId: UserId, roomId: string, socket: WebSocket) {
-
+        if (!this.rooms.get(roomId)) {
+            this.rooms.set(roomId, {
+                users: []
+            })
+        }
+        this.rooms.get(roomId)?.users.push({
+            id: userId,
+            name
+        })
     }
 
     removeUser(userId: UserId, roomId: string, socket: WebSocket) {
-        
+        const users = this.rooms.get(roomId)?.users;
+        if (users) {
+            users.filter(({ id }) => id !== userId);
+        }
     }
 }
